f00bf0144223495b5fb54cb121acbe5c4617cbc0
Teerapap Changwichukarn <teerapap.c@gmail.com>
2014-07-05 14:55:59 +0800

    Fix #66 : Make npm test to run and handle interactive debugger by itself

---
+++ test/runtest.js
@@ -0,0 +1,17 @@
+
+
+var nexpect = require('nexpect');
+
+nexpect.spawn("grunt", ["test"], { stripColors: true, verbose:true })
+       .wait(/  [0-9]*   var list = this;/)
+       .wait("debug> ")
+       .sendline("c")
+       .wait(/ [0-9]* };/)
+       .wait("debug> ")
+       .sendline("c")
+       .run(function (err, o, code) {
+          if (err) {
+            console.log(err)
+          }
+          process.exit(code);
+       });
