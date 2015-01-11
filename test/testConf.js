4fffcd5c6adc1b633ecb08777708fd7f2b5d0a1c
Teerapap Changwichukarn <teerapap.c@gmail.com>
2015-01-11 23:36:35 +0800

    Start chrome with arguments to be able to run tests on TravisCI

--- test/testConf.js
+++ test/testConf.js
@@ -48,7 +48,10 @@ exports.config = {
   // and
   // https://code.google.com/p/selenium/source/browse/javascript/webdriver/capabilities.js
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+    'chromeOptions': {
+      'args': ['no-sandbox','no-default-browser-check','no-first-run','disable-default-apps']
+    }
   },
 
   // A base URL for your application under test. Calls to protractor.get()
